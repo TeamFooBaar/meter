db2fa5226d493271d39acf95e7534c8ab4872aa3
Schaeff <schaeffer.thibaut@gmail.com>
2017-01-28 13:17:19 +0100

    add boilerplate for call to contract

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const request = require("request")
 const bodyParser = require("body-parser")
 
 const PORT = 3131
+const ETH_URL = "http://localhost:8545"
+const GROUND_CONTRACT_ADDRESS = ""
+const METER_ADDRESS = "0xcc30608bff4b93e46e08d9f141046f38b8005ece"
+
+const Web3 = require("web3")
+const web3 = new Web3(new Web3.providers.HttpProvider(ETH_URL))
 
 var requested = false;
 
@@ -11,13 +17,16 @@ app.use(bodyParser.json())
 
 function current(req, res) {
 	if (requested) {
-		return res.send("already requested")
+		return res.send("already")
 	}
-	switch (req.body.value < 0.2) {
+	switch (req.body.value < 0.3) {
 		case true:
 			{
-				requested = true;
-				return res.send("send drone")
+				requestDrone((err, result) => {
+					if(err) return res.send("drone request failed")
+					requested = true;
+					return res.send("drone request sent")
+				})
 				break;
 			}
 		default:
@@ -33,12 +42,20 @@ app.listen(PORT, () => {
 	console.log("Meter started!")
 })
 
+function requestDrone(cb) {
+	web3.eth.sendTransaction({from: METER_ADDRESS, to: METER_ADDRESS, value: 1, gas: 30000}, (err,res) => {
+		console.log(res)
+		console.log(err, res)
+		return cb(err, res);
+	})
+}
+
 // send a new value every second
 setInterval(() => {
 	request.post({
   uri: "http://localhost:" + PORT + "/current",
   method: 'POST',
   json: {value: Math.random()}
-}, (e, r, b) => {console.log(b)})
+}, (e, r, b) => {if(b !== "already") console.log(b)})
 }, 1000)
 
